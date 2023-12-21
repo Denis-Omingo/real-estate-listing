fe2492640c4ae77f71617421268f04c27bd59251
Denis Omingo <omingodenis7@gmail.com>
2023-12-21 16:26:19 +0300

    Create Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,14 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+
+        <Header/>
+
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
