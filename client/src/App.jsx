7afbccdce2482f38f90314334c4c7828a4474ecd
Denis Omingo <omingodenis7@gmail.com>
2023-12-21 15:49:48 +0300

    Create Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/sign-in" element={<Signin/>}/>
+          <Route path="/sign-up" element={<Signup/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+      Sign In Page
+      Sign In Page
+      Sign In Page
+      Sign In Page
+    </div>
+  )
+}
+
+export default Signin
