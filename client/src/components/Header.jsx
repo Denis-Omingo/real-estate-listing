b2374db88b37624e1eeac009ef8f38fa5963ee11
Denis Omingo <omingodenis7@gmail.com>
2023-12-22 10:42:52 +0300

    Complete Sign Up Page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
           <ul className='flex gap-4'>
             <Link to="/"><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
            <Link to="/about"> <li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-            <Link to="/sign-in"><li className='hidden sm:inline text-slate-700 hover:underline'>Sign In</li></Link>
+            <Link to="/sign-in"><li className=' text-slate-700 hover:underline'>Sign In</li></Link>
           </ul>
         </div>
 
