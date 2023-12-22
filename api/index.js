40d690c45cec8a3369267770c43c86414dac7a98
Denis Omingo <omingodenis7@gmail.com>
2023-12-22 15:40:31 +0300

    Craeting the frontend functionality of the sign up and sign in pages and also the backend functionality of sign in page

--- api/index.js
+++ api/index.js
@@ -23,9 +23,9 @@ app.listen(3000,()=>{
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || "Internal Server Error";
+app.use((error,req,res,next)=>{
+    const statusCode=error.statusCode || 500;
+    const message=error.message || "Internal Server Error Occured";
     return res.status(statusCode).json({
         success:false,
         statusCode,
