d2906fafb7f6d2308f27d3b8ec4361043feb4c6c
Denis Omingo <omingodenis7@gmail.com>
2023-12-25 21:18:28 +0300

    Complete Create Listing Page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -18,7 +19,8 @@ export default function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route element={<PrivateRoute/>}>
-          <Route path="/profile" element={<Profile/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+              <Route path="/create-listing" element={<CreateListing/>}/>
           </Route>
           <Route path="/sign-in" element={<Signin/>}/>
           <Route path="/sign-up" element={<Signup/>}/>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { app } from '../firebase'
 import { updateUserFailure, updateUserStart, updateUserSuccess, deleteUserStart, deleteUserSuccess, deleteUserFailure ,signOutUserFailure,
   signOutUserSuccess,
   signOutUserStart} from '../redux/user/userSlice'
+import { Link } from 'react-router-dom'
 
 
 export default function Profile() {
@@ -161,8 +162,13 @@ export default function Profile() {
 
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
         >{ loading? 'Updating...': 'Update'}</button>
+
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to='/create-listing'>
+        Create Listing
+      </Link>
     
       </form>
+     
 
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
