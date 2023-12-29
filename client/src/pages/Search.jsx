1849cf766ef6162d543961694b849b41ade757ff
Denis Omingo <omingodenis7@gmail.com>
2023-12-29 16:38:28 +0300

    updates in the import of ListingItem

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/Listingitem';
 
 
+
 export default function Search() {
   const navigate = useNavigate();
   const [sidebardata, setSidebardata] = useState({
