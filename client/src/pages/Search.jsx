4ddf2139057bd40124e428322812c30cad77a861
Denis Omingo <omingodenis7@gmail.com>
2023-12-29 16:52:57 +0300

    Search changes

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import ListingItem from '../components/Listingitem';
-
-
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
   const navigate = useNavigate();
